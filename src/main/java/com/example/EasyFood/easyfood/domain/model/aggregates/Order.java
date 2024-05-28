3efed240033f41c4d15ede9e60b03a29a24730f7
Juan Diego Huaman Monzón <u202123965@upc.edu.pe>
2024-05-28 17:39:35 -0500

    Add files

---
+++ src/main/java/com/example/EasyFood/easyfood/domain/model/aggregates/Order.java
@@ -0,0 +1,4 @@
+package com.example.EasyFood.easyfood.domain.model.aggregates;
+
+public class Order {
+}
---
+++ src/main/java/com/example/EasyFood/easyfood/domain/model/valueobjects/Lunch.java
@@ -0,0 +1,4 @@
+package com.example.EasyFood.easyfood.domain.model.valueobjects;
+
+public record Lunch() {
+}
