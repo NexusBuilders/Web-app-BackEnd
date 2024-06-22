71e916fa6e48cc1fdd74f8f3b9208048c86ea849
Juan Diego Huaman Monzón <u202123965@upc.edu.pe>
2024-06-22 02:08:29 -0500

    fix bugs

--- src/main/java/com/example/EasyFood/easyfood/infrastructure/persistence/jpa/JpaInventoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.EasyFood.easyfood.infrastructure.persistence.jpa;
-
-import com.example.EasyFood.easyfood.domain.model.entities.Inventory;
-import com.example.EasyFood.easyfood.domain.repository.InventoryRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaInventoryRepository extends JpaRepository<Inventory, Long>, InventoryRepository {
-}
